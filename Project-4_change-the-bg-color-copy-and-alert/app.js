a1dd95ea945d7a0bd69e36e3d08576e2b2562235
Berlin <evan1234.ek@gmail.com>
2023-02-05 10:12:48 +0600

    Done

--- Project-4_change-the-bg-color-copy-and-alert/app.js
+++ Project-4_change-the-bg-color-copy-and-alert/app.js
@@ -32,7 +32,17 @@ function main() {
       div.remove();
       div = null;
     }
-    generateToastMessage(`${output.value} copied!`);
+    if (isValidHex(output.value)) {
+      generateToastMessage(`${output.value} copied!`);
+    } else {
+      alert("Invalid Color Code");
+    }
+  });
+  output.addEventListener("keyup", function (e) {
+    const color = e.target.value;
+    if (color && isValidHex(color)) {
+      root.style.backgroundColor = color;
+    }
   });
 }
 
@@ -59,4 +69,15 @@ function generateToastMessage(msg) {
   });
 
   document.body.appendChild(div);
+}
+
+function isValidHex(color) {
+  if (color.length !== 7) {
+    return false;
+  }
+  if (color[0] !== "#") {
+    return false;
+  }
+  color = color.substring(1);
+  return /^[0-9A-Fa-f]{6}$/i.test(color);
 }
